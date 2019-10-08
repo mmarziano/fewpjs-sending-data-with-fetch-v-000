76d38364009dd1a006f6bb86b522a9bdf63504b1
Monica Marziano <mmarziano1@gmail.com>
2019-10-08 16:56:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ fetch(destinationURL, configurationObject)
 let destinationURL = 'http://localhost:3000/dogs';
 let configurationObject = {
   method: "POST",
-  
+  headers: {
+    "Content-Type": "application/json"
+    "Accept": "application/json"
+  }, 
+  body: JSON.stringify({
+    dogName: "Bryon",
+    dogBreed: "Poodle"
+  })
 }
