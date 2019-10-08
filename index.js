3c1b220e0f28437463f4e3ad145436e393d833c2
Monica Marziano <mmarziano1@gmail.com>
2019-10-08 16:57:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // Add your code here
-fetch(destinationURL, configurationObject)
-
 let destinationURL = 'http://localhost:3000/dogs';
-let configurationObject = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-    "Accept": "application/json"
-  }, 
-  body: JSON.stringify({
-    dogName: "Bryon",
-    dogBreed: "Poodle"
-  })
-};
+  let configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+      "Accept": "application/json"
+    }, 
+    body: JSON.stringify({
+      dogName: "Bryon",
+      dogBreed: "Poodle"
+    })
+  };
+
+fetch(destinationURL, configurationObject)
+  
